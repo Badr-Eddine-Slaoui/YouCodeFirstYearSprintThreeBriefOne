ba7dff5b12de3a0a4c502869e5e9a687d7108d7d
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-01 17:06:26 +0100

    Add Routing System

--- src/index.php
+++ src/index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/output.css">
-</head>
-<body>
-    <?php
-        echo "<h1 class='text-blue-600 text-3xl font-bold underline'>Test</h1>";
-    ?>
-</body>
-</html>
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
