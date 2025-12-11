08fc254928b3b13f827554307f5a21bc0e902a7d
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-11 22:12:48 +0100

    Add Migrations For Test

---
+++ src/database/Migrations/2025_12_11_120301_create_clients_table.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Migrations;
+
+use Foundations\DB\Migrations\Migration;
+use Foundations\DB\Migrations\Table;
+
+return new class extends Migration{
+    public function up(){
+        $this->create('clients', function(Table $table){
+            $table->id();
+            $table->string('name')->size(30);
+            $table->string('email')->size(150)->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    public function down(){
+        $this->dropIfExists('clients');
+    }
+};
+
+
