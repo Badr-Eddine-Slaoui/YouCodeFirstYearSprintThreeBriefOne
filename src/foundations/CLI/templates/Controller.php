7c9ccb91dc8e29a501947f987971c77da39c96a0
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-11 14:14:44 +0100

    Controller Template

---
+++ src/foundations/CLI/templates/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use Foundations\Controllers\Controller;
+use Foundations\Request\Request;
+
+class ControllerTemplate extends Controller {
+    public function index(Request $request) {
+        // Controller index
+    }
+}
