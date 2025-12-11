ad6f9e4f84b068be4b50eb14bd4d041830a26113
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-11 14:15:10 +0100

    Add Ressource Controller Template

---
+++ src/foundations/CLI/templates/ResourceController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+use Foundations\Controllers\Controller;
+use Foundations\Request\Request;
+
+class ResourceControllerTemplate extends Controller {
+    public function index(Request $request) {
+        // Controller index
+    }
+
+    public function store(Request $request) {
+        // Controller store
+    }
+
+    public function show(Request $request, int $id) {
+        // Controller show
+    }
+
+    public function edit(Request $request, int $id) {
+        // Controller edit
+    }
+
+    public function update(Request $request, int $id) {
+        // Controller update
+    }
+
+    public function destroy(Request $request, int $id) {
+        // Controller destroy
+    }
+}
