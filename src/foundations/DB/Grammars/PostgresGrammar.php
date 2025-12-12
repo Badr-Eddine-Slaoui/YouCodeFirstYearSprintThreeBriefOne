515f7779452b22878f62c075945dc4d180ac42e2
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-12 21:04:33 +0100

    Implement checkColumnUniqueSQL Function In PostgresGrammar Class

--- src/foundations/DB/Grammars/PostgresGrammar.php
+++ src/foundations/DB/Grammars/PostgresGrammar.php
@@ -232,6 +232,17 @@ class PostgresGrammar extends Grammar{
                 AND column_name = '$column';";
     }
 
+    public static function checkColumnUniqueSQL(string $table, string $column){
+        return "SELECT tc.constraint_name
+                FROM information_schema.table_constraints tc
+                JOIN information_schema.constraint_column_usage ccu 
+                    ON tc.constraint_name = ccu.constraint_name
+                WHERE tc.table_name = '$table'
+                AND tc.constraint_type = 'UNIQUE'
+                AND ccu.column_name = '$column';
+                ";
+    }
+
     public static function dropTableSQL(string $table) {
         return "DROP TABLE $table;";
     }
