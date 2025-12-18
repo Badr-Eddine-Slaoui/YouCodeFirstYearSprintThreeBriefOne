d0d2bfb440c9bff89bf4e0acf6388951aecd5dc7
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-18 22:09:47 +0100

    Add The pluck Method To Pluck Items Keys

--- src/foundations/DB/GoldDigger/Collection.php
+++ src/foundations/DB/GoldDigger/Collection.php
@@ -53,4 +53,11 @@ class Collection implements IteratorAggregate, Countable{
     {
         return new static(array_values(array_filter($this->items, $callback)));
     }
+
+    public function pluck(string $key): self
+    {
+        return new static(
+            array_map(fn($item) => $item->$key, $this->items)
+        );
+    }
 }
