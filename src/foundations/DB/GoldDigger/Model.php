636b949f98a58abc57022ee27eb88fd530b05281
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-18 17:03:03 +0100

    Add The lastOrFail Method To Find The Last Row Of A Query If Not Fail

--- src/foundations/DB/GoldDigger/Model.php
+++ src/foundations/DB/GoldDigger/Model.php
@@ -101,4 +101,8 @@ class Model{
     {
         return static::query()->where('id', $id)->last();
     }
+
+    public static function lastOrFail(int $id): ?static {
+        return static::query()->where('id', $id)->last() ?? abort(404, "{$id} not found");;
+    }
 }
