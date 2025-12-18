05ed3b1f329d08356e3d898c936d13a95e43ed6b
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-18 15:44:41 +0100

    Add The findOrFail Method To Find The First Row Of A Query If Not Fail

--- src/foundations/DB/GoldDigger/Model.php
+++ src/foundations/DB/GoldDigger/Model.php
@@ -81,4 +81,9 @@ class Model{
     {
         return static::query()->where('id', $id)->first();
     }
+
+    public static function findOrFail(int $id): ?static
+    {
+        return static::query()->where('id', $id)->first() ?? abort(404, "{$id} not found");
+    }
 }
