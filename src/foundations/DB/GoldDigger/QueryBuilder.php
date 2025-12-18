767db47599a2c3a33e970e0ee7892b5dec95ed1a
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-18 15:18:50 +0100

    Add Necessary Attributes To QueryBuilder Class

--- src/foundations/DB/GoldDigger/QueryBuilder.php
+++ src/foundations/DB/GoldDigger/QueryBuilder.php
@@ -3,5 +3,13 @@
 namespace Foundations\DB\GoldDigger;
 
 class QueryBuilder{
-    
+    protected string $model;
+    protected string $table;
+    protected array $selects = [];
+    protected array $wheres = [];
+    protected array $orWheres = [];
+    protected array $toUpdate = [];
+    protected bool $toDelete = false;
+    protected array $joins = [];
+    protected ?int $limit = null;
 }
