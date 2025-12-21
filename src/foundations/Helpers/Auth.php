d8b87011b3c3f819db3b8b3176e2eb573127b7d6
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-21 18:21:44 +0100

    Add The config Method To Load The Default Auth Config File

--- src/foundations/Helpers/Auth.php
+++ src/foundations/Helpers/Auth.php
@@ -5,5 +5,14 @@ namespace Foundations\Helpers;
 use Foundations\DB\GoldDigger\Model;
 
 class Auth{
-    
+    protected static array $config;
+
+    protected static function config(): array
+    {
+        if (!isset(static::$config)) {
+            static::$config = require __DIR__ . "/../../config/auth.php";
+        }
+
+        return static::$config;
+    }
 }
