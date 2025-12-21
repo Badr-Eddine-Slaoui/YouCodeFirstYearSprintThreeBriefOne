96d91ca39ae95aec38418673f0b33e4b0fb74162
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-21 18:22:09 +0100

    Add The login Method To The Auth Helper Class

--- src/foundations/Helpers/Auth.php
+++ src/foundations/Helpers/Auth.php
@@ -15,4 +15,39 @@ class Auth{
 
         return static::$config;
     }
+
+    public static function login(string $email, string $password): bool{
+        if(empty($email) || empty($password)){
+            return false;
+        }
+        
+        $config = static::config();
+        $guard = $config["defaults"]["guard"];
+        $driver = $config["guards"][$guard]["driver"];
+        $provider = $config["guards"][$guard]["provider"];
+        $providerDriver = $config["providers"][$provider]["driver"];
+        
+        if($providerDriver === "golddigger"){
+            $model = $config["providers"][$provider]["model"];
+
+            $user = $model::query()->where(["email" => $email])->first();
+
+            if(empty($user)){
+                return false;
+            }
+            
+            if(!password_verify($password, $user->password)){
+                return false;
+            }
+
+            if($driver == "session"){
+                Session::set("user_id", $user->id);
+                Session::regenerate();
+                return true;
+            }
+        }
+
+        return false;
+
+    }
 }
