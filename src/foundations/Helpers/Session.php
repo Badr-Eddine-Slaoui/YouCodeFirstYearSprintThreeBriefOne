7f4d3aeab0d38faa0a2df7c952defbb25ae2ca6d
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-21 17:24:09 +0100

    Add destroy Method To Destroy The Current Session

--- src/foundations/Helpers/Session.php
+++ src/foundations/Helpers/Session.php
@@ -73,4 +73,24 @@ class Session{
         $_SESSION = [];
     }
 
+    public static function destroy(){
+        static::start();
+        static::forget();
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        session_destroy();
+    }
+
 }
