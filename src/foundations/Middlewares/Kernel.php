0eadf8b4b453c750db9ec4a650d88dd3779f9ee9
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-21 20:40:19 +0100

    Add handle Method That Hnadle The Request And Create A New Pipeline And Pass That Request To It

--- src/foundations/Middlewares/Kernel.php
+++ src/foundations/Middlewares/Kernel.php
@@ -14,4 +14,12 @@ abstract class Kernel{
     public static function setMiddleware(string $middleware): void{
         self::$middleware[] = static::$middlewareAliases[$middleware] ?? $middleware;
     }
+    
+    public static function handle(Request $request, Closure $controller)
+    {
+        return (new Pipeline)
+            ->send($request)
+            ->through(array_merge(static::$globalMiddleware, self::$middleware))
+            ->then($controller);
+    }
 }
