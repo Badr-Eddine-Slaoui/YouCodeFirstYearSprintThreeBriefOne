94889beb84e8db5d9d3a91c23497bbe7d5a9276b
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-21 20:36:54 +0100

    Add  then Method In Pipeline Class That Should Execute All The Middlewares Then The Controller And Also Send Request Between them

--- src/foundations/Middlewares/Pipeline/Pipeline.php
+++ src/foundations/Middlewares/Pipeline/Pipeline.php
@@ -21,4 +21,19 @@ class Pipeline
         $this->middlewares = $middlewares;
         return $this;
     }
+
+    public function then(Closure $destination)
+    {
+        $pipeline = array_reduce(
+            array_reverse($this->middlewares),
+            function ($next, $middleware) {
+                return function ($request) use ($middleware, $next) {
+                    return (new $middleware)->handle($request, $next);
+                };
+            },
+            $destination
+        );
+
+        return $pipeline($this->request);
+    }
 }
