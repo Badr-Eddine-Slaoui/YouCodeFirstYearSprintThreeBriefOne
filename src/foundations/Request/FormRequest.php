769266c07cc39914323ad21c4294deaa45125b05
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2025-12-22 22:03:26 +0100

    Add validateRule Method To Validate One Attribute Rule

--- src/foundations/Request/FormRequest.php
+++ src/foundations/Request/FormRequest.php
@@ -20,4 +20,29 @@ abstract class FormRequest extends Request {
         return new Request();
     }
 
+    public static function validateRule(string $rule, $value): bool {
+        switch ($rule) {
+            case 'required':
+                return !empty($value);
+            case 'email':
+                return filter_var($value, FILTER_VALIDATE_EMAIL);
+            case 'string':
+                return is_string($value);
+            case 'int':
+                return is_int($value);
+            case str_contains($rule, 'min:'):
+                return strlen($value) >= (int) str_replace('min:', '', $rule);
+            case str_contains($rule, 'max:'):
+                return strlen($value) <= (int) str_replace('max:', '', $rule);
+            case str_contains($rule, "between:"):
+                $range = explode(',', str_replace('between:', '', $rule));
+                return strlen($value) >= (int) $range[0] && strlen($value) <= (int) $range[1];
+            case str_contains($rule,'in:'):
+                $enum = explode(',', str_replace('in:', '', $rule));
+                return in_array($value, $enum);
+            default:
+                return false;
+        }
+    }
+
 }
